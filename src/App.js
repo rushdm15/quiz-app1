c3972db11d16704a65ced728b30ebbd8d98d53fa
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-12-19 17:41:12 +0200

    manading data flow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import SetupForm from "./Form/SetupForm";
 import Loading from "./Loading/LoadingScreen";
 import Modal from "./Modal/Modal.jsx";
+import { useGlobalContext } from "./Context/Context.jsx";
 
 function App() {
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestions,
+    checkAnswer,
+  } = useGlobalContext();
+  if (waiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+
+  const { incorrect_answers, correct_answer, question } = question[index];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (temoIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[temp] = correct_answer;
+  }
   return (
     <main>
       <section className="quiz">
