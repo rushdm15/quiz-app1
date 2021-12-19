976746257d55f34eab85e559279cb60ce89398d6
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-12-19 17:58:41 +0200

    restructuring modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,10 +1,15 @@
+import { useGlobalContext } from "../Context/Context.jsx";
 const Modal =()=>{
+    const {modal, closeModal, correct, questions} = useGlobalContext();
     return(
-        <div className="modal-container isOpen">
+        <div 
+        className={`${modal?"modal-container isOpen" : "modal-container"}`}>
             <div className="modal-content">
                 <h2>congrats</h2>
-                <p> You answered n questions correctly </p>
-                <button className="close-btn">play again</button>
+                <p> 
+                  You answered {((correct/questions.length)*100).toFixed(0)}%
+                </p>
+                <button className="close-btn" onClick={closeModal}>play again</button>
             </div>
         </div>
     )
